1de5e3aa422a4ce61fb862f8676c358ba2872ce4
amefure <shachi10969@gmail.com>
2025-01-11 10:11:20 +0900

    Initial Commit

---
+++ HealthKitTest/HealthKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitTestApp.swift
+//  HealthKitTest
+//
+//  Created by t&a on 2025/01/11.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
